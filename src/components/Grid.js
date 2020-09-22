40f460005147ddbcef516527382c55af887a5f39
Alex Fiennes <alexfiennes246@gmail.com>
2020-09-22 14:54:04 +0100

    initial commit

---
+++ src/components/Grid.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Grid = () => {
+  return (
+    <div className="grid">
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+      <div className="square"></div>
+    </div>
+  );
+};
+
+export default Grid;
